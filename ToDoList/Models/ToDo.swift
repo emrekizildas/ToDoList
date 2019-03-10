fe58227bff7c2bf73a952ec2fe8f186856bee51b
emre <kizildas@icloud.com>
2019-03-10 12:23:45 +0300

    Created models files and ToDo class.

---
+++ ToDoList/Models/ToDo.swift
@@ -0,0 +1,19 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Emre on 3/10/19.
+//  Copyright © 2019 Emre Kızıldaş. All rights reserved.
+//
+
+import Foundation
+
+class ToDo {
+    var title: String
+    var content: String
+    
+    init(title: String, content: String) {
+        self.title = title
+        self.content = content
+    }
+}
