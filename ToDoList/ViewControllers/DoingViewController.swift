79df5eed71419b890124bfff9867c35e26a11e39
emre <kizildas@icloud.com>
2019-03-10 12:06:11 +0300

    ViewControllers added.

---
+++ ToDoList/ViewControllers/DoingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DoingViewController.swift
+//  ToDoList
+//
+//  Created by Emre on 3/10/19.
+//  Copyright © 2019 Emre Kızıldaş. All rights reserved.
+//
+
+import UIKit
+
+class DoingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ToDoList/ViewControllers/DoneViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DoneViewController.swift
+//  ToDoList
+//
+//  Created by Emre on 3/10/19.
+//  Copyright © 2019 Emre Kızıldaş. All rights reserved.
+//
+
+import UIKit
+
+class DoneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
